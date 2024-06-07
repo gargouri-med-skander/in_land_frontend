82568cb4cb13c317b8e07ae5ce012096519f06f6
gargouri-med-skander <mohamedskander.gargouri@esprit.tn>
2024-06-07 16:41:11 +0100

    implement  home  UI  ✨

--- src/app/public/components/home/home/home.component.ts
+++ src/app/public/components/home/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  screenWidth: any;
 
+  ngOnInit(): void {
+    this.onResize(null);
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth =
+      event != null ? event.target.innerWidth : window.innerWidth;
+  }
 }
