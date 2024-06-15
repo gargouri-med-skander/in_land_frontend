90b5096075fe959262382e39338de1b171c7619b
gargouri-med-skander <mohamedskander.gargouri@esprit.tn>
2024-06-15 04:18:34 +0100

    install primng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './public/components/base-layout/base-layout.component';
 import { NavBarComponent } from './shared/components/layout/nav-bar/nav-bar.component';
 import { FooterComponent } from './shared/components/layout/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { FooterComponent } from './shared/components/layout/footer/footer.compon
     NavBarComponent,
     FooterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
