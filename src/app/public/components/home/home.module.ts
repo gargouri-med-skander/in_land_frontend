e9aa6d3c6fe0a592927a046859fb4b9215aa3d98
gargouri-med-skander <mohamedskander.gargouri@esprit.tn>
2024-06-15 04:19:12 +0100

    implement animation on scroll

--- src/app/public/components/home/home.module.ts
+++ src/app/public/components/home/home.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
-
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, AnimateOnScrollModule],
 })
 export class HomeModule {}
