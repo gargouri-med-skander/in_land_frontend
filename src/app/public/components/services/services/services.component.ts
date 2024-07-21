20063b5ad7d1a40f5883e4aa1ccfe296b330519d
gargouri-med-skander <mohamedskander.gargouri@esprit.tn>
2024-07-21 21:08:04 +0100

    impl service page demo

--- src/app/public/components/services/services/services.component.ts
+++ src/app/public/components/services/services/services.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit {
+  markets = 0;
+  meetings = 0;
+  clientRevenue = 0;
+  salesPipeline = 0;
+  languages = 0;
 
+  ngOnInit(): void {
+    this.animateNumbers('markets', 20);
+    this.animateNumbers('meetings', 700);
+    this.animateNumbers('clientRevenue', 9);
+    this.animateNumbers('salesPipeline', 68);
+    this.animateNumbers('languages', 15);
+  }
+
+  private animateNumbers(
+    property:
+      | 'markets'
+      | 'meetings'
+      | 'clientRevenue'
+      | 'salesPipeline'
+      | 'languages',
+    target: number
+  ) {
+    let current = 0;
+    const increment = target / 100;
+    const interval = setInterval(() => {
+      if (current < target) {
+        current += increment;
+        this[property] = Math.round(current);
+      } else {
+        clearInterval(interval);
+      }
+    }, 10);
+  }
 }
